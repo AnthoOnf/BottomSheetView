0befa0fca00488f8cce0afaa0f61246eac87c2d9
Anthony <>
2019-03-05 11:46:25 +0100

    Bottom Sheet View Sample

---
+++ BottomSheetView/BottomSheetView.Test/ViewController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using BottomSheetView.Sources;
+using UIKit;
+
+namespace BottomSheetView.Test
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        partial void DynamicHeight_TouchUpInside(UIButton sender)
+        {
+            var controller = UIStoryboard.FromName("Main", null).InstantiateViewController("DynamicHeightController");
+
+            var bottomSheetViewController = new BottomSheetViewController(controller);
+
+            this.PresentModalViewController(bottomSheetViewController, false);
+        }
+
+        partial void FixedHeight_TouchUpInside(UIButton sender)
+        {
+            var controller = UIStoryboard.FromName("Main", null).InstantiateViewController("FixedHeightController");
+
+            SheetSize[] sheetSizes = { SheetSize.Fixed(300f) };
+            var bottomSheetViewController = new BottomSheetViewController(controller, sheetSizes);
+
+            this.PresentModalViewController(bottomSheetViewController, false);
+        }
+
+        partial void NavigationController_TouchUpInside(UIButton sender)
+        {
+            var controller = UIStoryboard.FromName("Main", null).InstantiateViewController("BottomSheetNavigationController");
+
+            SheetSize[] sheetSizes = { SheetSize.Fixed(300f) };
+            var bottomSheetViewController = new BottomSheetViewController(controller, sheetSizes);
+
+            this.PresentModalViewController(bottomSheetViewController, false);
+        }
+    }
+}
---
+++ BottomSheetView/BottomSheetView.Test/ViewController.designer.cs
@@ -0,0 +1,28 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace BottomSheetView.Test
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Action ("DynamicHeight_TouchUpInside:")]
+		partial void DynamicHeight_TouchUpInside (UIKit.UIButton sender);
+
+		[Action ("FixedHeight_TouchUpInside:")]
+		partial void FixedHeight_TouchUpInside (UIKit.UIButton sender);
+
+		[Action ("NavigationController_TouchUpInside:")]
+		partial void NavigationController_TouchUpInside (UIKit.UIButton sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
